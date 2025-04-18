61a73c63c090d61d5dd6dc8650f240a59cbfa13b
Falking3 <duncanreadle.20@gmail.com>
2025-04-18 14:54:35 +0100

    Basic script

---
+++ animgroups.py
@@ -0,0 +1,18 @@
+import bpy
+
+for action in bpy.data.actions:
+    group_list = []
+    for grp in action.layers[0].strips[0].channelbag(action.slots[0]).groups:
+        group_list.append(grp.name)
+    sorted_group_list = sorted(group_list, key=str.casefold)
+    for grp in sorted_group_list:
+        curves = []
+        grp_ref = action.layers[0].strips[0].channelbag(action.slots[0]).groups[grp]
+        for curve in grp_ref.channels:
+            curves.append(curve)
+        action.groups.remove(grp_ref)
+        newgrp = action.layers[0].strips[0].channelbag(action.slots[0]).groups.new(grp)
+        for curve in curves:
+            curve.group = newgrp
+
+                
